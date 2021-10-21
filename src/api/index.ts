eb6b0067f0d3978737625375ee433a3032b258c5
Ben Grynblat <benjamin.grynblat@gmail.com>
2021-10-21 16:49:16 +0100

    add cors preflight and body parser

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,18 +2,28 @@ import { respond } from "../common/ApiResponse";
 import { routes } from "./routes";
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 export const startup = (port: number = 3001) => {
   const app = express();
-
+  app.use(bodyParser.json());
   app.use((req: any, res: any, next: () => void) => {
-    res.set(`Access-Control-Allow-Origin`, (process.env.CORS_URL || "*").replace(/ /g, ""));
-    res.set(`Access-Control-Allow-Methods`, process.env.CORS_METHODS || "GET,PUT,POST,DELETE");
+    res.set(
+      `Access-Control-Allow-Origin`,
+      (process.env.CORS_URL || req.headers["origin"]).replace(/ /g, "")
+    );
+    res.set(
+      `Access-Control-Allow-Methods`,
+      process.env.CORS_METHODS || "GET, POST, DELETE, OPTIONS"
+    );
     res.set(`Access-Control-Allow-Headers`, process.env.CORS_HEADERS || "Content-Type");
     next();
   });
 
   routes(app);
+
+  // CORS Preflight
+  app.options("*", (req: any, res: any) => res.status(200).send());
 
   // 404 catch all
   app.use((req: any, res: any, next: () => void) => {
